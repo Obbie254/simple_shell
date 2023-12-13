2fe1b008f6f3d8bd7c65e981530e698a07c196d2
Obbie254 <obbienjehia@gmail.com>
2023-12-13 12:20:08 +0300

    added the prototypes: strcmp and cmd_execute fxns

--- main.h
+++ main.h
@@ -3,18 +3,18 @@
 
 #define PARTS_DELIMITER " \t\r\n\a\""
 
-/*----Libraries----*/
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-/*----Prototypes----*/
-/*--main.c--*/
 void interactive_shell(void);
 void none_interactive_shell(void);
 char *get_line(void);
 char **make_args(char *);
+int cmd_execute(char **);
+int _strcmp(char *, char *);
+
 
 #endif
